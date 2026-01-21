14cf13326b11f1b75084abe4e7625ba264825d5b
BharadwajPolusani <114689787+BharadwajPolusani@users.noreply.github.com>
2026-01-21 02:17:51 -0600

    Update MySpringProjectApplicationTests.java
    Commit 2

--- mySpringProject/src/test/java/com/example/mySpringProject/MySpringProjectApplicationTests.java
+++ mySpringProject/src/test/java/com/example/mySpringProject/MySpringProjectApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MySpringProjectApplicationTests {
 //test
+
+	//Hi How are you doing
 	@Test
 	void contextLoads() {
 	}
